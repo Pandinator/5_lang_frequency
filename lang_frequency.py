d39feb51f0310d3cbed009eddc53f1cf998d0589
Sergey Savtyra <drunkypanda0806@yandex.ru>
2016-12-02 01:16:08 +0400

    my solution

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,14 @@
+path = input() #User inputs a path to a file
 
+word_list = open(path, "r").read().split() #Open the file
 
-def load_data(filepath):
-    pass
-
-
-def get_most_frequent_words(text):
-    pass
-
-
-if __name__ == '__main__':
-    pass
+lst_sorted=sorted([ss for ss in set(word_list) if len(ss)>0],   #The term in squared brackets returns all unique strings 
+                   key=word_list.count,   						#in the list, which are not empty
+                   reverse=True)
+j = 1
+for word in lst_sorted: #print top-10 words
+	if j > 10:
+		break
+	else:
+		j += 1
+		print(word, end=", ")
