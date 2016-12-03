5c90c4f2999a29c27b6a4c8da19999a530dfda35
Sergey Savtyra <drunkypanda0806@yandex.ru>
2016-12-03 19:17:06 +0300

    Reworked construction and renamed variables
    Made the script consist of functions to be easer to be read
    Renamed variables, so they show the assignment

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,15 +1,27 @@
-path = input("Enter the path to the file: ") #User inputs a path to a file
+def load_data(filepath):
+    list_of_words = open(filepath, "r", encoding="utf-8").read().lower().split()
+    return list_of_words
+    #У меня не получилось вернуть список при использовании with open
 
-word_list = open(path, "r").read().split() #Open the file
 
-lst_sorted=sorted([ss for ss in set(word_list) if len(ss)>0],   #The term in squared brackets returns all unique strings 
-                   key=word_list.count,   						#in the list, which are not empty
-                   reverse=True)
-j = 1
-print("10 most frequent words in the text: ")
-for word in lst_sorted: #print top-10 words
-	if j > 10:
-		break
-	else:
-		j += 1
-		print(word, end=", ")
+def get_most_frequent_words(text):
+    list_sorted = sorted([add for add in set(text) if len(add)>0 ], key=text.count, reverse=True)
+    #"add" is a variable to add words in set if these are not empty
+    return list_sorted
+
+
+def print_only_10_common_words(list_to_get_words_from):
+    counter = 10
+    for word_print in list_to_get_words_from:
+    	if counter == 0:
+    		break
+    	else:
+    		counter -= 1
+    		print(word_print, end=", ")
+
+if __name__ == '__main__':
+    users_filepath = input("Enter the path to the file: \n")
+    words_list = load_data(users_filepath)
+    words_list = get_most_frequent_words(words_list)
+    print_only_10_common_words(words_list)
+
