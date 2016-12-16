84841b4b118344af0652b5416db0f0548f4fcddb
Sergey Savtyra <drunkypanda0806@yandex.ru>
2016-12-16 20:18:48 +0300

    Reworked the load_data function
    This is now "load_and_format_data" func and formats the words as it`s required despite the load of text, so that there is less code

--- lang_frequency.py
+++ lang_frequency.py
@@ -2,32 +2,27 @@ import re
 import collections
 
 
-def load_data(filepath):
+def load_and_format_data(filepath):
     with open(filepath, "r", encoding="utf-8") as words:
-        return words.read().lower().split()
-
-
-def format_data(data_to_format):
-    reworked_list_of_words = []
-    for word in data_to_format:  # remove any non-dig-alpha char
-        word = re.sub(r'[\W$]', '', word)
-        reworked_list_of_words.append(word)
-    return reworked_list_of_words
+        data = words.read()
+        data = re.sub(r'[\W]', ' ', data).split(r' ', data)
+        data = re.split(r' ', data)
+        return data
 
 
 def get_most_frequent_words(text):
-    list_sorted = collections.Counter(text)
-    return list_sorted
+    counted_words_in_dict = collections.Counter(text)
+    return counted_words_in_dict
 
 
-def print_only_10_common_words(list_to_get_words):
-    print("Top-10 common words: ")
-    for word_print, counter_of_printing in list_to_get_words.most_common(10):
+def print_most_common_words(get_words, words_amount):
+    print("Top-10 common words: \n")
+    for word_print, counter_of_printing in get_words.most_common(words_amount):
         print('%s: %d' % (word_print, counter_of_printing), end="; ")
+
 
 if __name__ == '__main__':
     users_filepath = input("Enter the path to the file: \n")
-    words_list = load_data(users_filepath)
-    words_list = format_data(words_list)
+    words_list = load_and_format_data(users_filepath)
     words_list = get_most_frequent_words(words_list)
-    print_only_10_common_words(words_list)
+    print_most_common_words(words_list, 10)
